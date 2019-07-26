d0098f258f511fd331139475258d6e4266c8fc0e
Stefaan Coussement <543534+stefaanc@users.noreply.github.com>
2019-07-26 16:10:55 +0100

    update license text

--- scripts/@PS_Prepare-KlusterEnvironment.ps1
+++ scripts/@PS_Prepare-KlusterEnvironment.ps1
@@ -5,6 +5,7 @@
 # more info: https://github.com/stefaanc/kluster
 #
 $STEPS_LOG_FILE = "$ROOT\logs\prepare-klusterenvironment_$( Get-Date -Format yyyyMMddTHHmmssffffZ ).log"
+$STEPS_LOG_APPEND = $false
 
 . "$(Split-Path -Path $script:MyInvocation.MyCommand.Path)/.steps.ps1"
 trap { do_trap }
