f59c2fed6dac83f3c454879cc373c052bb1ed91a
Stefaan Coussement <543534+stefaanc@users.noreply.github.com>
2019-10-18 13:04:30 +0100

    update to centos 7.7

--- packer/@PS_hyperv-build.ps1
+++ packer/@PS_hyperv-build.ps1
@@ -7,6 +7,8 @@
 
 $ErrorActionPreference='Stop'
 
+New-Item -ItemType Directory -Path $env:PACKER_ROOT\logs -Force
+
 packer build -color=false -on-error=ask -timestamp-ui -force -var-file $env:PACKER_ROOT/hyperv-variables.json $env:PACKER_ROOT/hyperv-1-setup.json
 packer build -color=false -on-error=ask -timestamp-ui -force -var-file $env:PACKER_ROOT/hyperv-variables.json $env:PACKER_ROOT/hyperv-2-centos7.json
 packer build -color=false -on-error=ask -timestamp-ui -force -var-file $env:PACKER_ROOT/hyperv-variables.json $env:PACKER_ROOT/hyperv-3-nethserver7.json
